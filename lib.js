bb8bfa235d50b7e7554d59a04542442374caab34
Jung Eun (Jessica) Kwak <jkwak39@gmail.com>
2020-02-05 15:42:47 +0800

    #010203

--- lib.js
+++ lib.js
@@ -1,5 +1,8 @@
 module.exports = {
 	toSquare: data => {
 		return typeof data == 'object' ? data.filter(e => typeof e == 'number').map(e => e * e) : typeof data == 'number' ? data * data : NaN
+	},
+	toPower: (a, b) => {
+		return typeof a == 'number' && typeof b == 'number' ? Math.pow(a, b) : NaN
 	}
 }
